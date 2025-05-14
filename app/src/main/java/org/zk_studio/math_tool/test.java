647f08c551b5d075b0c442e21606e350537adbb4
张睿绅 <14060132+minecraft_rayson@user.noreply.gitee.com>
2025-05-14 22:10:46 +0800

    first commit

---
+++ app/src/main/java/org/zk_studio/math_tool/test.java
@@ -0,0 +1,35 @@
+package org.zk_studio.math_tool;
+
+import static androidx.core.content.ContextCompat.startActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class test extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_test);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Button button = findViewById(R.id.button2);
+        button.setOnClickListener(this::onClick);
+    }
+
+    private void onClick(View v) {
+        finish();
+    }
+}
